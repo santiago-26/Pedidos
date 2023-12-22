fa2630277a7db2c196fb4e797543d5dc5ad4d1d6
Mauricio Santiago <mauriciosantiago95@hotmail.com>
2023-12-21 23:20:36 -0300

    primeiro commit

---
+++ src/entities/Client.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.util.Date;
+
+public class Client {
+	
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	
+	public Client() {
+	}
+
+
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+
+}
---
+++ src/entities/Order.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client; 
+	private List<OrderItem> itens = new ArrayList<>();
+	
+	public Order() {
+		super();
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		super();
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+	
+	
+}
